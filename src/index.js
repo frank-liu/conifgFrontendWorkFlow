dd2a9f2d99f9ed22ba632fb8fdab71e5b6bc019b
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-20 23:29:12 +0100

    add husky pre-commit hook

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: Peng Liu
+ * @Date: 2021-05-20 21:55:26
+ * @LastEditTime: 2021-05-20 23:22:21
+ * @FilePath: /conifgFrontendWorkFlow/src/index.js
+ */
+console.log("hello  " + "world");
