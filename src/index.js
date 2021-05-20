2a3352cc9f4dc05812950b6fa56b13ec03efa5ef
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-20 23:49:29 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-20 23:36:46
+ * @LastEditTime: 2021-05-20 23:49:17
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
-console.log("hello  " + "worl d");
+console.log("he llo  " + "worl d");
