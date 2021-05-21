0bbf2f8d870b05b30a37c5bed92b250f1c7d9e1e
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-21 22:12:30 +0100

    feat: dfsfdsf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-20 23:49:17
+ * @LastEditTime: 2021-05-21 22:05:54
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
-console.log("he llo  " + "worl d");
+console.log("he l lo" + "worl d")
+console.log("run   lint-staged")
