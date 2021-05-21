8040ea91dfadb7a9f4166cf67a2c79b9da677a34
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-21 22:19:04 +0100

    refactor: change 1 line

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-21 22:13:57
+ * @LastEditTime: 2021-05-21 22:18:37
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
 console.log("he l lo" + "worl d")
-console.log("run   lint-staged")
+console.log("run   lint-s taged")
