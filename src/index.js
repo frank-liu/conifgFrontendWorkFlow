144114f45a30816bf691dfe717e558446ebc9e33
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-21 22:21:06 +0100

    refactor(index.js): change one line to test
    I changed a line in this js file to test the commit message in github.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-21 22:18:37
+ * @LastEditTime: 2021-05-21 22:20:05
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
 console.log("he l lo" + "worl d")
-console.log("run   lint-s taged")
+console.log("run   lint-  s taged")
