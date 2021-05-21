1fb9a2b76dbaa7ad823be0c44570a50430ebcc72
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-05-21 22:14:34 +0100

    refactor(index.js): add one line

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-21 22:05:54
+ * @LastEditTime: 2021-05-21 22:13:57
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
 console.log("he l lo" + "worl d")
