c016bf7ecc271f8e92cb4f0b43f0db1128a469a1
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-07-11 14:19:39 +0100

    add words after line10.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-07-11 14:10:39
+ * @LastEditTime: 2021-07-11 14:19:37
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
 console.log("he l lo" + "worl d")
 console.log("run   lint-  s taged")
 console.log("run   l  i  nt-  s taged")
-console.log("add a new line")
+console.log("add a new line. see if there is a conflict.")
