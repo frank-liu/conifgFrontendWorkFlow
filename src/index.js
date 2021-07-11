5e82721acc692ebda0b858915836b12a41d44795
iStorage UK <istorageuk@iStorages-MacBook-Pro.local>
2021-07-11 14:10:55 +0100

    add a new line

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 /*
  * @Author: Peng Liu
  * @Date: 2021-05-20 21:55:26
- * @LastEditTime: 2021-05-22 21:59:00
+ * @LastEditTime: 2021-07-11 14:10:39
  * @FilePath: /conifgFrontendWorkFlow/src/index.js
  */
 console.log("he l lo" + "worl d")
 console.log("run   lint-  s taged")
 console.log("run   l  i  nt-  s taged")
+console.log("add a new line")
